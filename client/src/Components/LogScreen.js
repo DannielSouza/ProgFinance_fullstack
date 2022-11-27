113174ab826b464380ac5db66f43552b52374fd2
Daniel  Souza <104663666+DannielSouza@users.noreply.github.com>
2022-11-27 12:11:07 -0300

    logscreen removed

--- client/src/Components/LogScreen.js
+++ client/src/Components/LogScreen.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Register from './Register'
 import Login from './Login'
-import style from './styles/LogScreen.module.css'
 
 const LogScreen = () => {
   const [loginOrRegister, setLoginOrRegister] = React.useState(true)
