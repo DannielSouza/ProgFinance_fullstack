03b1cf4d34e028a57645c80d831fc156d569b715
Daniel  Souza <104663666+DannielSouza@users.noreply.github.com>
2022-11-27 09:36:55 -0300

    db message change

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,15 +1,11 @@
 const {Sequelize} = require('sequelize')
 
+/* Connect your db here */
+
 const sequelize = new Sequelize('financeproject', 'root', 'password',{
   hosst: 'localhost',
   dialect: 'mysql'
 })
 
-/* try {
-  sequelize.authenticate()
-  console.log("Conectado ao banco de dados.")
-} catch (error) {
-  console.log("Erro ao conectar ao banco de dados: " + error)
-} */
 
 module.exports = sequelize
